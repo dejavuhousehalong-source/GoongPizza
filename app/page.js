e4eafb3bcd2fb7185ecd3c0d9a1669990eb0242e
dejavuhousehalong-source <dejavuhouse.halong@gmail.com>
2026-03-26 11:48:52 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -44,7 +44,17 @@ const areas = {
   useEffect(() => {
     if (date && time) fetchBookings()
   }, [date, time])
+function addMinutes(time, mins) {
+  const [h, m] = time.split(':').map(Number)
+  const date = new Date()
+  date.setHours(h)
+  date.setMinutes(m + mins)
 
+  const hh = String(date.getHours()).padStart(2, '0')
+  const mm = String(date.getMinutes()).padStart(2, '0')
+
+  return `${hh}:${mm}`
+}
   async function fetchBookings() {
     const { data } = await supabase
       .from('reservations')
